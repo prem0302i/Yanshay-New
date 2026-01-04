e968a25b444cf9e7440cc738a445c8708017b2d5
prem0302i <sonawaneprem11243@gmail.com>
2026-01-04 16:34:09 +0530

    From prem

---
+++ app/account/settings/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Settings</h1>
+      <p>Manage your account settings here.</p>
+    </div>
+  );
+};
+
+export default SettingsPage;
